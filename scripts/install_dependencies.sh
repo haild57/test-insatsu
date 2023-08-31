dafb10dd32db1922330e3322f8d7e78fd29df985
T-Hle <le_dinh_hai@tokai-grp.co.jp>
2023-08-31 20:20:54 +0900

    change code

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 sudo yum install tomcat -y
-sudo rm -r /usr/share/tomcat/webapps/*
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,3 +3,4 @@ isExistApp="$(pgrep tomcat)"
 if [[ -n $isExistApp ]]; then
 sudo systemctl stop tomcat.service
 fi
+sudo rm -r /usr/share/tomcat/webapps/*
