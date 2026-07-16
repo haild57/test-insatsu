e1e07d348b4212c994bff01ddbd3350b49c184ea
ＬＥ　ＤＩＮＨ　ＨＡＩ 【COM】 SD事業部ｼｽﾃﾑ技術部一課 <t153980@tgr-ad.local>
2026-07-16 11:00:18 +0900

    test rollback

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-exit 1
 sudo rm -f /usr/share/sample-java-app.war
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-exit 1
 sudo systemctl stop tomcat.service
 sudo pkill -f sample-java-app.war
